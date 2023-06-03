514efe5942ec645bf68b7476ea3361d131defb24
Jeffrey840 <jeffreycabrera93@gmail.com>
2023-06-02 20:58:04 -0700

    completed removeVowels

---
+++ removeVowels.js
@@ -0,0 +1,12 @@
+function disemvowel(str) {
+    const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+    let result = "";
+  
+    for (let i = 0; i < str.length; i++) {
+      if (!vowels.includes(str[i])) {
+        result += str[i];
+      }
+    }
+  
+    return result;
+  }
