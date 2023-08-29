29a8da87fda2293f96ff46e62c584aa93b294e94
Jeffrey840 <jeffreycabrera93@gmail.com>
2023-08-28 21:33:36 -0700

    solved cw question

---
+++ dropCap.js
@@ -0,0 +1,9 @@
+function dropCap(str) {
+    return str.split(' ').map(word => {
+        if (word.length > 2) {
+            return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+        } else {
+            return word;
+        }
+    }).join(' ');
+}
